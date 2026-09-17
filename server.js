b8eb86b152e50f01dbad7a2ab9e8b82960b603d9
abubakargoni939-eng <abubakargoni939@gmail.com>
2026-09-17 11:07:32 +0100

    Set up Express server with static file serving

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+import express from 'express';
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
