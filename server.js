64ba8d77862153ad8599e432d24fbf246673b336
abubakargoni939-eng <abubakargoni939@gmail.com>
2026-09-20 12:40:26 +0100

    Refactor server.js for static file serving

--- server.js
+++ server.js
@@ -1,38 +1,14 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('README.mdpublic'));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/admin.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "admin.html"));
-});
-
-app.get("/shop.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "shop.html"));
-});
-
-app.get("/profile.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "profile.html"));
-});
-
-app.get("/api/health", (req, res) => {
-  res.json({
-    ok: true,
-    app: "Quick Marketplace"
-  });
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/README.mdpublic/index.html');
 });
 
 app.listen(PORT, () => {
